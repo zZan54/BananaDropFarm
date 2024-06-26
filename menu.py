3998a862bfef18901e88be5b50bcf2038235d6b8
Zan <59426552+zZan54@users.noreply.github.com>
2024-06-26 19:19:43 +0200

    Add CPS spoofing feature, update pointer address, and enhance info display
    - Added functionality to spoof CPS
    - Updated the pointer address to the latest value
    - Improved the display of information for better clarity and usability

--- menu.py
+++ menu.py
@@ -34,12 +34,15 @@ try:
 except Exception:
     pass
 
+score_addr = gameModule + 0x00E68E00
+score_offsets = [0x90, 0x78, 0x8F8, 0x28, 0x20, 0xB8, 0x830]
+
 def changescore():
     new_score = int(scorevalue.get())
     try:
-        game.write_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]), new_score)
-    except Exception:
-        print("An error occurred while trying to set the score.")
+        game.write_int(GetPtrAddr(score_addr, score_offsets), new_score)
+    except Exception as e:
+        print("An error occurred while trying to set the score.", e)
         exit()
 
 def botidlecheckbypass():
@@ -52,9 +55,9 @@ def botidlecheckbypass():
         def update_score():
             try:
                 while botidlecheckbypass_var.get():
-                    current_score = game.read_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]))
+                    current_score = game.read_int(GetPtrAddr(score_addr, score_offsets))
                     new_score = current_score + random.randint(1, 25)
-                    game.write_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]), new_score)
+                    game.write_int(GetPtrAddr(score_addr, score_offsets), new_score)
                     time.sleep(botidlecheckbypassdelay)
             except Exception:
                 print("An error occurred while trying to bypass the bot idle check.")
@@ -65,7 +68,7 @@ def botidlecheckbypass():
             try:
                 while botidlecheckbypass_var.get():
                     new_score = random.randint(1, 1000000)
-                    game.write_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]), new_score)
+                    game.write_int(GetPtrAddr(score_addr, score_offsets), new_score)
                     time.sleep(botidlecheckbypassdelay)
             except Exception:
                 print("An error occurred while trying to bypass the bot idle check.")
@@ -75,15 +78,57 @@ def botidlecheckbypass():
         def update_score():
             try:
                 while botidlecheckbypass_var.get():
-                    current_score = game.read_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]))
+                    current_score = game.read_int(GetPtrAddr(score_addr, score_offsets))
                     new_score = current_score + 1
-                    game.write_int(GetPtrAddr(gameModule + 0x00E5C150, [0x40, 0x318, 0xB8, 0x0, 0x50, 0x10, 0x30]), new_score)
+                    game.write_int(GetPtrAddr(score_addr, score_offsets), new_score)
                     time.sleep(botidlecheckbypassdelay)
             except Exception:
                 print("An error occurred while trying to bypass the bot idle check.")
                 pass
 
     thread = threading.Thread(target=update_score)
+    thread.daemon = True
+    thread.start()
+
+def spoofcps():
+    if spoofcpsdelay_var.get() != None:
+        spoofcpsdelay = float(spoofcpsdelay_var.get())
+    else:
+       spoofcpsdelay = 0.5
+
+    if spoofcpsmethod_var.get() == "Random":
+        def update_cps():
+            try:
+                while spoofcps_var.get():
+                    new_cps = random.randint(1, 1000000)
+                    game.write_int(GetPtrAddr(score_addr, score_offsets) + 0x10, new_cps)
+                    time.sleep(spoofcpsdelay)
+            except Exception:
+                print("An error occurred while trying to spoof cps.")
+                pass
+
+    elif spoofcpsmethod_var.get() == "Random normal":
+        def update_cps():
+            try:
+                while spoofcps_var.get():
+                    new_cps = random.randint(1, 20)
+                    game.write_int(GetPtrAddr(score_addr, score_offsets) + 0x10, new_cps)
+                    time.sleep(spoofcpsdelay)
+            except Exception:
+                print("An error occurred while trying to spoof cps.")
+                pass
+
+    elif spoofcpsmethod_var.get() == "Static":
+        def update_cps():
+            try:
+                while spoofcps_var.get():
+                    game.write_int(GetPtrAddr(score_addr, score_offsets) + 0x10, 15)
+                    time.sleep(spoofcpsdelay)
+            except Exception:
+                print("An error occurred while trying to spoof cps.")
+                pass
+
+    thread = threading.Thread(target=update_cps)
     thread.daemon = True
     thread.start()
 
@@ -123,17 +168,51 @@ botidlecheckbypassdelay = customtkinter.CTkComboBox(master=botidlecheckbypassall
 botidlecheckbypassdelay.pack(side="left", padx=5)
 botidlecheckbypassdelay_var.set('2')
 
-scorechangerinfo = customtkinter.CTkLabel(master=options.tab("Info"), text="Score Changer - changes score", fg_color="transparent", text_color="white")
-scorechangerinfo.pack(padx=20, pady=8, anchor=customtkinter.CENTER)
+spoofcpsall = customtkinter.CTkFrame(master=options.tab("Cheat"))
+spoofcpsall.pack(side="top", padx=20, pady=8)
 
-botidlecheckbypassinfo = customtkinter.CTkLabel(master=options.tab("Info"), text="Bot Idle Check Bypass - bypasses the bot idle check depending on \nwhat method you chose", fg_color="transparent", text_color="white")
-botidlecheckbypassinfo.pack(padx=20, pady=8, anchor=customtkinter.CENTER)
+spoofcps_var = customtkinter.BooleanVar(value=False)
+spoofcps = customtkinter.CTkCheckBox(master=spoofcpsall, text="Spoof Cps", command=spoofcps, variable=spoofcps_var)
+spoofcps.pack(side="left", padx=5)
 
-botidlecheckbypassmethodsinfo = customtkinter.CTkLabel(master=options.tab("Info"), text="Bypass methods: \nRandom Increment - adds a random number from 1 to 25 to the current score \nRandom value - changes the current score to a random value \nIncrement - adds 1 to the current score", fg_color="transparent", text_color="white")
-botidlecheckbypassmethodsinfo.pack(padx=20, pady=8, anchor=customtkinter.CENTER)
+spoofcpsmethod_var = customtkinter.StringVar(value="Random normal")
+spoofcpsmethod = customtkinter.CTkComboBox(master=spoofcpsall, values=["Random", "Random normal", "Static"], variable=spoofcpsmethod_var, width=150)
+spoofcpsmethod.pack(side="left", padx=5)
+spoofcpsmethod_var.set("Random normal")
 
-notes = customtkinter.CTkLabel(master=options.tab("Info"), text=f"Notes: \nThe chosen method is activated every few seconds, depending on the delay you choose. \nDefault delay is 2 seconds. You need to click for the score to update.", fg_color="transparent", text_color="white")
-notes.pack(padx=20, pady=8, anchor=customtkinter.CENTER)
+spoofcpsdelay_var = customtkinter.StringVar(value='0.5')
+spoofcpsdelay = customtkinter.CTkComboBox(master=spoofcpsall, values=['0.5', '1', '2', '5', '10', '15', '30', '45', '60', '120', '180', '300'], variable=spoofcpsdelay_var, width=60)
+spoofcpsdelay.pack(side="left", padx=5)
+spoofcpsdelay_var.set('0.5')
+
+info = """Score Changer - changes score
+
+Bot Idle Check Bypass - bypasses the bot idle check depending on what 
+method you chose
+
+Spoof Cps - spoofes the cps value depending on what method you chose
+
+Bypass methods: 
+ - Random Increment - adds a random number from 1 to 25 to 
+   the current score 
+ - Random value - changes the current score to a random value 
+ - Increment - adds 1 to the current score
+
+Spoof methods:
+ - Random - sets a random cps value
+ - Random normal - sets a random cps value from 1 to 20
+ - Static - sets a static cps value of 15
+
+Notes: 
+ - The chosen method is activated every few seconds, 
+   depending on the delay you choose. 
+ - Default delay is 2 seconds.
+ - You need to click for the score to update.
+"""
+infobox = customtkinter.CTkTextbox(master=options.tab("Info"), height=230, width=460)
+infobox.insert("0.0", info)
+infobox.configure(state="disabled")
+infobox.pack(padx=20, pady=8, anchor=customtkinter.CENTER)
 
 githublink = customtkinter.CTkLabel(app, text="github.com/zZan54", fg_color="transparent", text_color="white")
 githublink.place(x=220, y=310)
